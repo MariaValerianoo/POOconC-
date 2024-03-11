48271fbd97a4557aed7ee3f04cd7efc7e60a73da
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-03-11 15:05:44 -0500

    Update Empleado.cpp

--- Salud/Empleado.cpp
+++ Salud/Empleado.cpp
@@ -7,6 +7,8 @@ class Empleado:public Persona{
     string cargo;
     int valorHora;
     int horasTrabajadas;
+    int fechaNacimiento;
+    int año;
     double honorarios;
     double honorarios2;
     void setDepartamento(string _departamento){
@@ -53,14 +55,85 @@ class Empleado:public Persona{
         return horasTrabajadas;
 
     }
+    void setFechaNacimiento(int _fechaNacimiento){
+
+        fechaNacimiento = _fechaNacimiento;
+
+    }
+
+    int getFechaNacimiento(){
+
+        return fechaNacimiento;
+
+    }
+    void setAño(int _año){
+
+        año = _año;
+
+    }
+
+    int getAño(){
+
+        return año;
+
+    }
+    void setHonorarios(double _honorarios){
+
+        honorarios = _honorarios;
+
+    }
+
+    int getHonorarios(){
+
+        return honorarios;
+
+    }
+
     public:
+    void pedirDatos(){
+        cout<<"En cual departamento trabajas?:  "<<endl;
+        cin>>departamento;
+        cout<<"Cual es el cargo que tienes?:  "<<endl;
+        cin>>cargo;
+        cout<<"Cual es el cargo que tienes?:  "<<endl;
+        cin>>cargo;
+        cout<<"Cuanto vale la hora?:  "<<endl;
+        cin>>valorHora;
+        cout<<"Cuantas horas trabajaste?:  "<<endl;
+        cin>>horasTrabajadas;
+        cout<<"En que año naciste?:  "<<endl;
+        cin>>fechaNacimiento;
+        cout<<"Cual es el año actual?:  "<<endl;
+        cin>>año;
+
+    };
+
     void calcularHonorarios(){
         double reteica=0.00966;
-        honorarios=horasTrabajadas*valorHora;
-        honorarios2=honorarios-reteica
-    }; 
+        honorarios=(horasTrabajadas*valorHora)-((horasTrabajadas*valorHora)*reteica);
+    };
+    int mayorEdad(int fechaNacimiento, int año){
+        int edad;
+        edad=año-fechaNacimiento;
+        if (edad>=18){
+            cout<<"Eres mayor de edad:)";
+        }else{
+            cout<<"Eres menos de edad:("<<endl;
+        }
+    }
+    void mostrarDatos(){
+        Persona mostrarPersonal();
+        cout<<"Tu cargo es: "<<cargo<<endl;
+        cout<<"Tus horas trabajadas son: "<<horasTrabajadas<<endl;
+        cout<<"Tus honorarios son: "<<honorarios<<endl;
+    }
 
-};
-int main(){
+    };
+    
 
-}
+/*int main(){
+    Empleado pedro;
+    pedro.mostrarDatos();
+    pedro.mayorEdad(int fechaNacimiento, int año);
+
+}*/
